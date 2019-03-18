2e5c6b9f4de59a376bff8b32f0b4ff4df310c6d1
Rusichka <41853541+Rusichka@users.noreply.github.com>
2019-03-18 12:11:43 +0200

    sample code
    //sample code to write 100 random ints to a file, 1 per line

--- src/test/java/com/gamepoint/example/ExampleApplicationTests.java
+++ src/test/java/com/gamepoint/example/ExampleApplicationTests.java
@@ -4,6 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import java.io.PrintStream;
+import java.io.IOException;
+import java.io.File;
+
+import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,3 +20,20 @@ public class ExampleApplicationTests {
 
 }
 
+public class WriteToFile
+{	public static void main(String[] args)
+	{	try
+		{	PrintStream writer = new PrintStream( new File("randInts.txt"));
+			Random r = new Random();
+			final int LIMIT = 100;
+
+			for(int i = 0; i < LIMIT; i++)
+			{	writer.println( r.nextInt() );
+			}
+			writer.close();
+		}
+		catch(IOException e)
+		{	System.out.println("An error occured while trying to write to the file");
+		}
+	}
+}
